bf28b5389be79f055b536ab9a8e2e5bfe745bfbf
tchesa <cesarolimpio@gmail.com>
2022-03-17 21:23:02 -0300

    timer

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import LayoutSelector, { Layout } from '../../components/layout-selector/layout-selector'
+import Timer from '../../components/timer/timer'
 import './main.scss'
 
 const Main = () => {
   const [layout, setLayout] = useState<Layout>(Layout.GRID)
+  const [startedAt] = useState(new Date())
+
   return <div className="main-container">
     <div className="meeting-col">
       <div className="meeting-header">
         <LayoutSelector value={layout} onChange={setLayout} />
+        <Timer startedAt={startedAt} />
       </div>
       <div className="meeting-content">
 
